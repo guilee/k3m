e370859238c8282f286ac749eec720e6d4e96edf
Guillaume <guilee@hadouken>
2014-05-06 02:47:45 +0200

    New method to retrieve a sinogram list (file) from a ressource (for instance kanjivg but could be used for others ressources)

--- scripts/extract_strokes/print_kanji.py
+++ scripts/extract_strokes/print_kanji.py
@@ -1,15 +1,43 @@
+# -*- coding: utf-8 -*-
 import argparse
 import sys
+import shutil
+import os
+import codecs
+
 
 def retrieveFileFromChar(char):
-    char_code = char.decode('utf-8')
+    if isinstance(char, unicode):
+        char_code = char
+    else:
+        char_code = char.decode('utf-8')
     return '%s.svg' %  hex(ord(char_code)).replace('x', '')
+
+def retrieveHanjaFromRessources(charListFile, resourcePath, outputPath):
+    if not(os.path.exists(outputPath)):
+        os.makedirs(outputPath)
+
+    not_found = open(os.path.join(outputPath, 'not_found.txt'), 'w')    
+
+    with codecs.open(charListFile, 'r', 'utf-8') as f_d:
+        for i, char in enumerate(f_d.readlines()):
+            print type(char)
+            filename = retrieveFileFromChar(char[0])
+            src = os.path.join(resourcePath, filename)
+            dst = os.path.join(outputPath, '%s-%s' % (i+1, filename))
+            try:
+                shutil.copy(src, dst)
+            except IOError as err:
+                not_found.write(char.encode('utf-8'))
+
+
 
 if __name__ == '__main__':
     # Options
     parser = argparse.ArgumentParser(description='Find right kanji file given a character')
-    parser.add_argument("-c", required=True, help="Chinese character to search")
-    parser.add_argument("-r", required=False, help="Path of repertory for chinese character (svg files)")
+    parser.add_argument("-l", required=True, help="List of chinese character to search")
+    parser.add_argument("-r", required=True, help="Path of repertory for chinese character (svg files)")
+    parser.add_argument("-o", required=True, help="Path of output repertory")
     opt = parser.parse_args(sys.argv[1:])
 
-    print retrieveFileFromChar(opt.c)
+    retrieveHanjaFromRessources(opt.l, opt.r, opt.o)
